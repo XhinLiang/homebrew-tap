c38278d522b8f785c76f8f4183e186fce5b63119
xhinliang <xhinliang@gmail.com>
2017-07-22 14:28:48 +0800

    fix

--- Formula/vi2048.rb
+++ Formula/vi2048.rb
@@ -8,27 +8,14 @@ class Vi2048 < Formula
   url "https://github.com/XhinLiang/vi2048/archive/v1.0.0.tar.gz"
   sha256 "e107a66ad835775a44e3479cc735c389a75d93b72c83c1ce6c80610b5c0a9b52"
 
-  depends_on "cmake" => :build
+  depends_on "cmake"
 
   def install
     # ENV.deparallelize  # if your formula fails when building in parallel
 
     # Remove unrecognized options if warned by configure
-    system "cmake", "."
+    system "cmake", ".", *std_cmake_args
     system "make"
     system "make", "install" # if this fails, try separate make/make install steps
-  end
-
-  test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test vi2048`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "true"
   end
 end
