4a6a35a49fc3a177d2bc1eec9da0498fea8f8603
liangxuhua <liangxuhua@kuaishou.com>
2019-02-16 00:14:07 +0800

    release new version persister

--- Formula/persister.rb
+++ Formula/persister.rb
@@ -5,8 +5,8 @@
 class Persister < Formula
   desc "persist the foreign image of markdown files."
   homepage "https://github.com/XhinLiang/persister"
-  url "https://github.com/XhinLiang/persister/releases/download/1.0.0/persister"
-  sha256 "cff6c4c57db6402bb9464a3f42f28a9b5d080063d7538852cfc4d65d9fe0d9fc"
+  url "https://github.com/XhinLiang/persister/releases/download/1.0.1/persister"
+  sha256 "cb38863ac34654f4836d72c8c396a24cdbcaa44046c27e0d1a1cc956c6e0451b"
 
   def install
     # ENV.deparallelize  # if your formula fails when building in parallel
